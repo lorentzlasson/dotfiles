2019719eeb0cb0ebe12c8da5ab36d6e91129ea4d
Lorentz Lasson <lasson.lorentz@gmail.com>
2024-06-14 22:55:07 +0200

    enable other treesitter modules

--- .config/nvim/lua/language-support.lua
+++ .config/nvim/lua/language-support.lua
@@ -146,6 +146,21 @@ require 'nvim-treesitter.configs'.setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      -- TODO: figure out keymaps
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+
+  indent = {
+    enable = true
+  }
 }
 
 -- SYMBOL OUTLINE
