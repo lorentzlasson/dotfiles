98db8753b9453abcf7afe3209fa52a5656c2f62a
Lorentz Lasson <lasson.lorentz@gmail.com>
2024-07-06 14:23:36 +0200

    extract packages file

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,6 +1,11 @@
 { pkgs, lib, ... }:
 
 {
+  imports =
+    [
+      ./packages.nix
+    ];
+
   system.stateVersion = "23.11";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -153,47 +158,4 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [
-    alacritty
-    bat
-    curl
-    dbeaver-bin
-    dropbox-cli
-    eza
-    file
-    gcc
-    gptfdisk
-    graphite-cli
-    jq
-    lsof
-    lua-language-server
-    nerdfonts
-    nil
-    nixpkgs-fmt
-    nodePackages_latest.bash-language-server
-    nodePackages_latest.typescript-language-server
-    ollama
-    pavucontrol
-    pinta
-    protonvpn-gui
-    pspg
-    ripgrep
-    shellcheck
-    sqls
-    stow
-    tree
-    unison-ucm
-    vscode-langservers-extracted
-    wget
-    xclip
-    yaml-language-server
-    zed-editor
-    zoxide
-
-    # unfree
-    discord
-    google-chrome
-    slack
-    signal-desktop
-  ];
 }
---
+++ nix/packages.nix
@@ -0,0 +1,47 @@
+{ pkgs,  ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    alacritty
+    bat
+    curl
+    dbeaver-bin
+    dropbox-cli
+    eza
+    file
+    gcc
+    gptfdisk
+    graphite-cli
+    jq
+    lsof
+    lua-language-server
+    nerdfonts
+    nil
+    nixpkgs-fmt
+    nodePackages_latest.bash-language-server
+    nodePackages_latest.typescript-language-server
+    ollama
+    pavucontrol
+    pinta
+    protonvpn-gui
+    pspg
+    ripgrep
+    shellcheck
+    sqls
+    stow
+    tree
+    unison-ucm
+    vscode-langservers-extracted
+    wget
+    xclip
+    yaml-language-server
+    zed-editor
+    zoxide
+
+    # unfree
+    discord
+    google-chrome
+    slack
+    signal-desktop
+  ];
+}
