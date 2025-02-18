efe16c7c6ef18ad46f6f8b1407f61d434f29fa2a
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-02-18 21:27:33 +0100

    add dunkel todo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,7 @@ o.termguicolors = true
 cmd 'colorscheme dunkel-kontrast'
 
 -- make horizontal splits clearer
+-- TODO: with dunkel?
 cmd [[ highlight StatusLineNC guibg=black guifg=white ]]
 cmd [[ highlight StatusLine guibg=black guifg=#7a59ff ]]
 
