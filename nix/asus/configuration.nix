45806fdbf21a924f13e27c7f454cba1b028b5be3
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-04-13 23:10:44 +0200

    install blocky on asus

--- nix/asus/configuration.nix
+++ nix/asus/configuration.nix
@@ -6,7 +6,10 @@
       ./hardware-configuration.nix
     ];
 
-  networking.hostName = "asus";
+  networking = {
+    hostName = "asus";
+    nameservers = [ "127.0.0.1" ];
+  };
 
   # https://nixos.wiki/wiki/Nvidia
   hardware = {
@@ -31,6 +34,32 @@
       enable = true;
       openFirewall = true;
       user = "lorentz";
+    };
+
+    blocky = {
+      enable = true;
+      settings = {
+        ports = {
+          dns = 53; # Port for incoming DNS Queries.
+          http = 4000;
+        };
+        upstreams.groups.default = [
+          "https://one.one.one.one/dns-query" # Using Cloudflare's DNS over HTTPS server for resolving queries.
+        ];
+        # For initially solving DoH/DoT Requests when no system Resolver is available.
+        bootstrapDns = {
+          upstream = "https://one.one.one.one/dns-query";
+          ips = [ "1.1.1.1" "1.0.0.1" ];
+        };
+        blocking = {
+          blackLists = {
+            ads = [ "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" ];
+          };
+          clientGroupsBlock = {
+            default = [ "ads" ];
+          };
+        };
+      };
     };
   };
 }
