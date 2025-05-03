7c63725bb3e052abc4acb1c72426db48c84ca98e
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-05-03 12:24:06 +0200

    break out wanted avante deps to root deps

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -9,10 +9,12 @@ require("lazy").setup({
   { "neovim/nvim-lspconfig" },
   { "nvim-tree/nvim-tree.lua" },
   { "nvim-tree/nvim-web-devicons" },
+  { "nvim-telescope/telescope.nvim" },
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
   },
+  { "MeanderingProgrammer/render-markdown.nvim" },
   { "simrat39/symbols-outline.nvim" },
   { "stevearc/dressing.nvim" },
   { "stevearc/oil.nvim" },
@@ -53,10 +55,8 @@ require("lazy").setup({
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
+    version = false,
     opts = {
-      -- add any opts here
-      -- for example
       provider = "openai",
       openai = {
         endpoint = "https://api.openai.com/v1",
@@ -64,48 +64,14 @@ require("lazy").setup({
         timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
         temperature = 0,
         max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
       },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",        -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
     },
   }
 })
