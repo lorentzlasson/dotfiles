d35c0acaefb8c230e9d149de47fa1e1b7031c5b3
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-05-25 20:12:56 +0000

    setup nginx on nuc

--- nix/nuc/configuration.nix
+++ nix/nuc/configuration.nix
@@ -15,6 +15,20 @@
   services = {
     openssh.enable = true;
 
+    nginx = {
+      enable = true;
+      virtualHosts."grafana.lorentz.casa" = {
+        locations."/" = {
+          proxyPass = "http://localhost:3000/";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          '';
+        };
+      };
+    };
+
     # Point router dns to the ip of this computer
     blocky = {
       enable = true;
