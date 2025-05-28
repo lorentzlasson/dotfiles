8a737ea7f6c1c3ad921ef2d3ab83c9a9d4d9e3e7
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-05-28 15:53:58 +0200

    allow nvim tree to grow further

--- .config/nvim/lua/mod-nvim-tree.lua
+++ .config/nvim/lua/mod-nvim-tree.lua
@@ -1,3 +1,9 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  view = {
+    width = {
+      max = 100,
+    },
+  },
+})
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>nf', ':NvimTreeFindFile<CR>', { noremap = true })
