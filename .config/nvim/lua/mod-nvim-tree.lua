b87c514be519f8491abb49ab2295b1e225bca5f2
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-05-31 18:10:20 +0200

    remap W to close only to cursor

--- .config/nvim/lua/mod-nvim-tree.lua
+++ .config/nvim/lua/mod-nvim-tree.lua
@@ -1,9 +1,22 @@
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set("n", "W", api.tree.collapse, opts("Collapse"))
+end
+
 require("nvim-tree").setup({
   view = {
     width = {
       max = 100,
     },
   },
+  on_attach = on_attach
 })
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>nf', ':NvimTreeFindFile<CR>', { noremap = true })
