7415ffd7d6d58e338e4209305a522103dabd1f62
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-06-15 11:00:09 +0200

    uninstall avante

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -51,26 +51,4 @@ require("lazy").setup({
     'nvim-dunkel',
     url = 'https://codeberg.org/fabrlyn/dunkel.nvim.git',
   },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false,
-    opts = {
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o",  -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      },
-    },
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-  }
 })
