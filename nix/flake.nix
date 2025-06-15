748ed9fb1638e37c810796a498db0606aedf15cd
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-06-15 12:25:22 +0200

    dry root flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,37 +3,22 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }: {
-    nixosConfigurations.xps15 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./xps15/configuration.nix
-        ./pc/configuration.nix
-      ];
+  outputs = { nixpkgs, ... }:
+    let
+      mkSystem = hostConfig: baseConfig: nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          hostConfig
+          baseConfig
+        ];
+      };
+    in
+    {
+      nixosConfigurations = {
+        xps15 = mkSystem ./xps15/configuration.nix ./pc/configuration.nix;
+        xps13 = mkSystem ./xps13/configuration.nix ./pc/configuration.nix;
+        asus = mkSystem ./asus/configuration.nix ./pc/configuration.nix;
+        nuc = mkSystem ./nuc/configuration.nix ./server/configuration.nix;
+      };
     };
-
-    nixosConfigurations.xps13 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./xps13/configuration.nix
-        ./pc/configuration.nix
-      ];
-    };
-
-    nixosConfigurations.asus = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./asus/configuration.nix
-        ./pc/configuration.nix
-      ];
-    };
-
-    nixosConfigurations.nuc = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./nuc/configuration.nix
-        ./server/configuration.nix
-      ];
-    };
-  };
 }
