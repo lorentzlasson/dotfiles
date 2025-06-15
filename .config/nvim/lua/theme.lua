9f0f5b05baa60e84e16c02686e2e4f6155c4971b
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-06-15 14:34:01 +0200

    rm purple separators

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,8 +1,3 @@
 local cmd = vim.cmd
 
 cmd 'colorscheme dunkel-kontrast'
-
--- make horizontal splits clearer
--- TODO: with dunkel?
-cmd [[ highlight StatusLineNC guibg=black guifg=white ]]
-cmd [[ highlight StatusLine guibg=black guifg=#7a59ff ]]
