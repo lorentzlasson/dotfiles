de7082434b9a33edede55788d8d73e29b6d047df
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-06-16 21:18:39 +0200

    rm old unused shell functions

--- .config/shell/functions.sh
+++ .config/shell/functions.sh
@@ -1,16 +1,8 @@
-vimconflicts() {
-  vim "$(git diff --name-only --diff-filter=U | xargs)"
-}
-
 gitbranchauthors() {
   git remote update origin --prune > /dev/null;
   for branch in $(git branch -r | grep -v HEAD); do echo "$(git show --format=%an "$branch" | head -n 1): $branch"; done |
     sort -r |
     column -s ":" -t
-}
-
-gitcheckout() {
-  git remote show origin | grep "HEAD branch" | sed 's/.*: //' | xargs -I '{}' git checkout -b "$1" '{}'
 }
 
 # e.g.
@@ -35,40 +27,6 @@ process_using_port() {
 
 publicip() {
   curl https://ipinfo.io/ip
-}
-
-gitpullfilefromlastcommit() {
-  msg=$(git log -1 --pretty=%B)
-  git reset --soft HEAD~
-  git reset "$1"
-  git commit --message "$msg"
-}
-
-gitpr() {
-    MAIN_BRANCH=$(git remote show origin | grep "HEAD branch" | sed 's/.*: //')
-    BRANCH_NAME=$(git symbolic-ref -q HEAD | sed 's/.*\///')
-    ISSUE_NR=$(git symbolic-ref -q HEAD | sed 's/.*-//')
-
-    HEADER=''
-    if [ "$1" != "" ]; then
-        # Use message from command line
-        HEADER="$1"
-    else
-        # Default to branch name
-        HEADER="$BRANCH_NAME"
-    fi
-
-    MESSAGE=$(echo -e "$HEADER\n\ncloses #$ISSUE_NR")
-
-    #Check if issue nr exists
-    re='^[0-9]+$'
-    if ! [[ $ISSUE_NR =~ $re ]] ; then
-        MESSAGE="$HEADER"
-    fi
-
-    git push -u origin "$BRANCH_NAME"
-    hub pull-request -b "$MAIN_BRANCH" -m "$MESSAGE"
-    echo "PR for $BRANCH_NAME created"
 }
 
 # https://unix.stackexchange.com/a/112284
