ec8e985c8356613a1ce93cdaa8d1a6549d8796ad
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-07-17 13:46:39 +0200

    enable pwr managerment
    hoping it solves suspend wake issue

--- nix/asus/configuration.nix
+++ nix/asus/configuration.nix
@@ -15,8 +15,7 @@
     graphics.enable = true;
     nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = false;
-      powerManagement.finegrained = false;
+      powerManagement.enable = true;
       open = false;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.beta;
