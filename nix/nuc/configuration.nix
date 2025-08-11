6db7a5651d151246fefa11a9a39a424fcaec7ece
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-08-11 21:34:18 +0200

    add ihn domain

--- nix/nuc/configuration.nix
+++ nix/nuc/configuration.nix
@@ -52,6 +52,19 @@
             '';
           };
         };
+        "ihate.news" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://localhost:7000";
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+            '';
+          };
+        };
         "www.lorentz.casa" = {
           enableACME = true;
           forceSSL = true;
