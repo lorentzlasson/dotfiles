f2ef26c6384807c5d9bdb49ab4ce6e912607eccd
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-09-06 19:21:15 +0200

    force nvidia in steam

--- nix/asus/configuration.nix
+++ nix/asus/configuration.nix
@@ -10,6 +10,13 @@
   networking.hostName = "asus";
   system.stateVersion = "23.11";
 
+  environment.sessionVariables = {
+    __NV_PRIME_RENDER_OFFLOAD = "1";
+    __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    __VK_LAYER_NV_optimus = "NVIDIA_only";
+  };
+
   # https://nixos.wiki/wiki/Nvidia
   hardware = {
     graphics.enable = true;
