7e81d734c860b22fbb0461eda700f61e783945b9
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-09-28 15:10:01 +0200

    reinstall asus
    with support for larger boot partition

--- nix/asus/hardware-configuration.nix
+++ nix/asus/hardware-configuration.nix
@@ -5,42 +5,35 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "thunderbolt" "xhci_pci" "nvme" "ahci" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "thunderbolt" "xhci_pci" "nvme" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/7bf89bc1-ad8c-4d82-b05b-fa6101a82ed3";
+    {
+      device = "/dev/disk/by-uuid/8cf3a931-d09d-4f9b-9f8d-da6210678218";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/7424-C920";
+    {
+      device = "/dev/disk/by-uuid/9F96-780D";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
+  fileSystems."/efi" =
+    {
+      device = "/dev/disk/by-uuid/7424-C920";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
   swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-1dc7907fc880.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-2223634c2781.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-25b7df687fb8.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-5f740dd85e6c.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-94731e26e100.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-b0a043f64fc5.useDHCP = lib.mkDefault true;
-  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp50s0u2u4.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp95s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
