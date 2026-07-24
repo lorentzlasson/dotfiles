b8f7166a178148772b0501c865b07f52880d2ab1
Lorentz Lasson <lasson.lorentz@gmail.com>
2026-07-24 10:56:14 +0200

    check indentation style across all files
    static-qa now fails when any tracked file uses a tab for indentation, not just .gitconfig

--- scripts/static-qa.sh
+++ scripts/static-qa.sh
@@ -8,3 +8,4 @@ deadnix --fail --exclude nix/*/hardware-configuration.nix nix/
 shellcheck --shell=bash --exclude=SC1090,SC1091,SC2034,SC2086,SC2155 nix/**/*.sh .config/shell/*.sh .zshrc
 shfmt --indent 2 --diff .config/shell/*.sh .zshrc
 stylua --check .config/nvim/
+editorconfig-checker
